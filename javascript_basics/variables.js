f67f25f371526753ca58130ccbfa8fd11bae8bd8
wlhmmxrtz <moritz.wilhelmi@student.htldornbirn.at>
2022-10-18 12:13:42 +0200

    moved counter files and created variables.js

---
+++ javascript_basics/variables.js
@@ -0,0 +1,16 @@
+let a = 17.4;
+let b = 3.9;
+
+
+result = a/b;
+
+console.log(result);
+
+console.log("Das ergebnis von " + a + " geteilt druch " + b + " ergiebt " + result);
+
+
+let firstname = "Moritz";
+
+let lastname = "Wilhelmi";
+
+console.log("Mein Name ist " + firstname + " " + lastname);
