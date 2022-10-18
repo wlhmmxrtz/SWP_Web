95cbf8fa7c309f815d4143a5c56d93cee6889e28
wlhmmxrtz <moritz.wilhelmi@student.htldornbirn.at>
2022-10-18 12:34:41 +0200

    made the code a bit prettier

--- javascript_basics/variables.js
+++ javascript_basics/variables.js
@@ -1,13 +1,13 @@
 let a = 17.4;
 let b = 3.9;
 
-
-result = a/b;
+result = a / b;
 
 console.log(result);
 
-console.log("Das ergebnis von " + a + " geteilt druch " + b + " ergiebt " + result);
-
+console.log(
+  "Das ergebnis von " + a + " geteilt druch " + b + " ergiebt " + result
+);
 
 let firstname = "Moritz";
 
