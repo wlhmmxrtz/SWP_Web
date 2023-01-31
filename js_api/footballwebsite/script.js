0d40bb6f4171ee2d122051c1d16f9f35b3c74d6a
Kfue-bit <68454422+Kfue-bit@users.noreply.github.com>
2023-01-31 18:04:45 +0100

    minor changes to code

--- js_api/footballwebsite/script.js
+++ js_api/footballwebsite/script.js
@@ -1,28 +1,27 @@
-
 function getGameday() {
     let gameday = document.getElementById("input").value;
 
 
-const apiurl = "https://api.openligadb.de/getmatchdata/legaseriea/2018/" + gameday;
+    const apiurl = "https://api.openligadb.de/getmatchdata/legaseriea/2018/" + gameday;
 
-function loadTable() {
-    fetch(apiurl).then((result) => {
-        result.json().then((data) => {
-            outputTable(data)
+    function loadTable() {
+        fetch(apiurl).then((result) => {
+            result.json().then((data) => {
+                outputTable(data)
+            })
         })
-    })
-}
+    }
 
-function outputTable(data) {
-    let html = "";
-    
-    data.forEach((match) => {
-        if (match.matchIsFinished) {
-          html += `<div class="matches"><img class="teamIMG" src="${match.team1.teamIconUrl}" alt="Team Icon"> <p class="result">${match.matchResults[0].pointsTeam1} : ${match.matchResults[0].pointsTeam2}</p><img class="teamIMG" src="${match.team2.teamIconUrl}" alt="Team Icon"></div>`;
-        }
-      });
-    document.getElementById("standings").innerHTML=html;
-}
+    function outputTable(data) {
+        let html = "";
 
-loadTable()
+        data.forEach((match) => {
+            if (match.matchIsFinished) {
+                html += `<div class="matches"><img class="teamIMG" src="${match.team1.teamIconUrl}" alt="Team Icon"> <p class="result">${match.matchResults[0].pointsTeam1} : ${match.matchResults[0].pointsTeam2}</p><img class="teamIMG" src="${match.team2.teamIconUrl}" alt="Team Icon"></div>`;
+            }
+        });
+        document.getElementById("standings").innerHTML = html;
+    }
+
+    loadTable()
 }
