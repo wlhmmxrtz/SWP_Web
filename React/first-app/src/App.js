c06dc3d75ff00bb070c18b2e95cbf59412c44f0e
Kfue-bit <68454422+Kfue-bit@users.noreply.github.com>
2023-03-07 17:15:23 +0100

    Finished the input and made a uri generator

--- React/first-app/src/App.js
+++ React/first-app/src/App.js
@@ -1,26 +1,74 @@
 import logo from "./logo.png";
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import SpotifyPlayer from "react-spotify-web-playback";
 import 'react-spotify-auth/dist/index.css'
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  console.log("Inputvalue is " + inputValue);
+
+  let randomURI="";
+
+  let randomNumber = Math.floor(Math.random() * 10)
+
+  let roundedNumber = Math.round(randomNumber);
+
+  switch (roundedNumber) {
+    case 1:
+      randomURI="spotify:album:4Uv86qWpGTxf7fU7lG5X6F"
+      break;
+    case 2:
+      randomURI="spotify:album:5ll74bqtkcXlKE7wwkMq4g"
+      break;
+    case 3:
+      randomURI="spotify:album:4SZko61aMnmgvNhfhgTuD3"
+      break;
+    case 4:
+      randomURI="spotify:album:3WFTGIO6E3Xh4paEOBY9OU"
+      break;
+    case 5:
+      randomURI="spotify:album:20r762YmB5HeofjMCiPMLv"
+      break;
+    case 6:
+      randomURI="spotify:album:0OcMap99vLEeGkBCfCwRwS"
+      break;
+    case 7:
+      randomURI="spotify:album:7D2NdGvBHIavgLhmcwhluK"
+      break;
+    case 8:
+      randomURI="spotify:album:7gsWAHLeT0w7es6FofOXk1"
+      break;
+    case 9:
+      randomURI="spotify:album:6pwuKxMUkNg673KETsXPUV"
+      break;
+    case 10:
+      randomURI="spotify:album:5CnpZV3q5BcESefcB3WJmz"
+      break;
+  }
+
+  console.log(randomNumber);
+
   return (
     <div className="App">
       <header className="App-header">
-      <script src="https://kit.fontawesome.com/20f56f52ff.js" crossorigin="anonymous"></script>
-        <h1 class="h1">Get your code <a data-component-name="Anchor" rel="noopener noreferrer" target="_blank" href="https://accounts.spotify.com/en/authorize?response_type=token&amp;client_id=adaaf209fb064dfab873a71817029e0d&amp;redirect_uri=https:%2F%2Fdeveloper.spotify.com%2Fdocumentation%2Fweb-playback-sdk%2Fquick-start%2F&amp;scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state&amp;show_dialog=true" class="here"><span>here</span></a></h1>
+        <h2 class="headline">The Kanye Player</h2>
         <div>
-        <input type="text"/>
-        <button><i class="fa-solid fa-check fa-2x"></i></button>
+        <input type="text" id="codeInput" value={inputValue} placeholder="Spotify Token here" onInput={handleInputChange}/>
         </div>
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1 class="h1">No Token? - Get yours <a data-component-name="Anchor" rel="noopener noreferrer" target="_blank" href="https://accounts.spotify.com/en/authorize?response_type=token&amp;client_id=adaaf209fb064dfab873a71817029e0d&amp;redirect_uri=https:%2F%2Fdeveloper.spotify.com%2Fdocumentation%2Fweb-playback-sdk%2Fquick-start%2F&amp;scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state&amp;show_dialog=true" class="here"><span>here</span></a></h1>
+        <img src={logo} className="App-logo" alt="logo"/>
           <div className="SpotifyPlayer">
             <SpotifyPlayer
               className="App-spotify-player"
-              token="BQAU0xq5y5rrtNTtznaUw3nrhT-kVMMF2Mi3GUMtUItUfb9-uzP2M7MFlEjJATjGtRsXFDASAjSeGEXqKSX-VlDxUoAPsodbo3YL1ou2auGIK8IzoDx3Qu1sJh6OIFnoQsKv7KjRQOYowrdDHgARIGl_ZdkWl8_zCMxZltumJf63N-lpP4RqLGWG8qcPqhrVL3dIcK8ucmIQQOi4s75VShQqakDQRU3KgTI
-              "
-              uris={["spotify:album:20r762YmB5HeofjMCiPMLv"]}
+              hideAttribution="true"
+              token={inputValue}
+              uris={[randomURI]}
               styles={{
                 activeColor: "#fff",
                 bgColor: "#faebd7",
@@ -37,6 +85,5 @@ function App() {
     </div>
   );
 }
-
 export default App;
 
